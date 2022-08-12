4472360b2785c3a442369b08446f2d6c41816823
joacogm12 <gomezjoaquin991@gmail.com>
2022-08-12 00:43:27 -0500

    added more projects and a carousel

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const numCarousel = document.getElementById("numCarousel")
-
 var slidePosition = 1;
 SlideShow(slidePosition);
 
@@ -22,5 +20,75 @@ function SlideShow(n) {
         slides[i].style.display = "none";
     }
     slides[slidePosition - 1].style.display = "block";
-    numCarousel.textContent = (`${slidePosition} / ${slides.length}`)
+}
+
+
+
+var slidePositionFront = 1;
+SlideShowFront(slidePositionFront);
+
+// forward/Back controls
+function plusSlidesFront(n, backFront) {
+    SlideShowFront(slidePositionFront += n, backFront);
+}
+
+//  images controls
+function currentSlideFront(n) {
+    SlideShowFront(slidePositionFront = n);
+}
+
+function SlideShowFront(n, backFront) {
+    var i;
+    var slides = document.getElementsByClassName("frontContainers");
+    if (n > slides.length) { slidePositionFront = 1 }
+    if (n < 1) { slidePositionFront = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    if (slidePositionFront == slides.length && backFront == false) {
+        slidePositionFront = 3
+    }
+    if (slidePositionFront == slides.length && backFront == true) {
+        slidePositionFront = 1
+    }
+    slides[slidePositionFront - 1].style.display = "block";
+    slides[slidePositionFront].style.display = "block";
+}
+
+
+var slidePositionBack = 1;
+SlideShowBack(slidePositionBack);
+
+// forward/Back controls
+function plusSlidesBack(n, backFront) {
+    SlideShowBack(slidePositionBack += n, backFront);
+}
+
+//  images controls
+function currentSlideBack(n) {
+    SlideShowBack(slidePositionBack = n);
+}
+
+function SlideShowBack(n, backFront) {
+    var i;
+    var slides = document.getElementsByClassName("BackContainers");
+    if (n > slides.length) { slidePositionBack = 1 }
+    if (n < 1) { slidePositionBack = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    if (slidePositionBack == slides.length && backFront == true) {
+        slidePositionBack = 1
+    }
+
+    if (slidePositionBack == slides.length && backFront == false) {
+        slidePositionBack = 3
+    }
+    if (slidePositionBack == slides.length && backFront == true) {
+        slidePositionBack = 1
+    }
+    slides[slidePositionBack - 1].style.display = "block";
+    slides[slidePositionBack].style.display = "block";
 } 
